e5967d81cd048a3f9fb24a798aacd456a23b45cd
Mathavan <smaddy799@gmail.com>
2025-04-02 16:30:46 +0530

    Created Terraform Files

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# Environment Variables
+variable "region" {
+  description = "Region to create resources"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+

32bdfe161ee0d0420290b82e0afb3cba58eddcb9
Mathavan <smaddy799@gmail.com>
2025-04-03 12:15:03 +0530

    Created VPC File

--- variables.tf
+++ variables.tf
@@ -14,3 +14,41 @@ variable "environment" {
   type        = string
 }
 
+# VPC Variables
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "public_subnet_AZ1-CIDR" {
+  description = "Public Subnet AZ1 CIDR block"
+  type        = string
+}
+
+variable "public_subnet_AZ2-CIDR" {
+  description = "Public Subnet AZ2 CIDR block"
+  type        = string
+}
+
+variable "private_app_subnet_AZ1-CIDR" {
+  description = "Private App Subnet AZ1 CIDR block"
+  type        = string
+}
+
+variable "private_app_subnet_AZ2-CIDR" {
+  description = "Private App Subnet AZ2 CIDR block"
+  type        = string
+}
+
+
+variable "private_data_subnet_AZ1-CIDR" {
+  description = "Private Data Subnet AZ1 CIDR block"
+  type        = string
+}
+
+variable "private_data_subnet_AZ2-CIDR" {
+  description = "Private Data Subnet AZ2 CIDR block"
+  type        = string
+}
+
+
